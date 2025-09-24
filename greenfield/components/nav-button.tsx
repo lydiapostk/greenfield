b8456f69527a1bbb6526cbd9c1206450b65af248
Lydia HE (HTX) <lydia_he@htx.gov.sg>
2025-09-25 01:28:21 +0800

    Changed navigation button light mode to be teal colour

--- greenfield/components/nav-button.tsx
+++ greenfield/components/nav-button.tsx
@@ -4,22 +4,24 @@ interface NavButtonProps {
     title: string;
     href: string;
     mode?: "light" | "dark";
+    className?: string;
 }
 
 const colourCSS = {
-    light: "text-black bg-stone-200/75 hover:bg-stone-200/25",
+    light: "text-black bg-teal-100/25 hover:bg-teal-100/50",
     dark: "text-white bg-indigo-800/25 hover:bg-indigo-800/75",
 };
 
 export default function NavButton({
     title,
     href,
+    className,
     mode = "light",
 }: NavButtonProps) {
     return (
         <a
             href={href}
-            className={`${colourCSS[mode]} rounded-full shadow-md shadow-slate-500/40 hover:shadow-slate-500/25 flex items-center justify-center font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]`}
+            className={`${colourCSS[mode]} rounded-full shadow-md shadow-slate-500/40 hover:shadow-slate-500/25 flex items-center justify-center font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px] ${className}`}
         >
             {title}
         </a>
