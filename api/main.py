526879a2ba33b57bc1bac4625a29640984582e4c
Lydia HE (HTX) <lydia_he@htx.gov.sg>
2025-09-24 20:19:17 +0800

    API\Removed API function that allows direction addition of startup record

--- api/main.py
+++ api/main.py
@@ -4,24 +4,21 @@ from sqlmodel import Session, select
 from api.database import init_db, get_session
 from api.models import Startup
 
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     init_db()  # Create tables on startup
-    yield      # Run the app
+    yield  # Run the app
     # Cleanup logic (if any) on shutdown
+
 
 app = FastAPI(title="Startups API", lifespan=lifespan)
 
-@app.post("/startups/", response_model=Startup)
-def create_startup(startup: Startup, session: Session = Depends(get_session)):
-    session.add(startup)
-    session.commit()
-    session.refresh(startup)
-    return startup
 
 @app.get("/startups/", response_model=list[Startup])
 def list_startups(session: Session = Depends(get_session)):
     return session.exec(select(Startup)).all()
+
 
 @app.get("/startups/{startup_id}", response_model=Startup)
 def get_startup(startup_id: int, session: Session = Depends(get_session)):
