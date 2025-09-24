91a1b4d66bce9d8c807b93bb4b03db095095076b
Lydia HE (HTX) <lydia_he@htx.gov.sg>
2025-09-24 20:30:49 +0800

    API\Created function that will invoke GPT-5-mini to search up a start-up from url

--- api/main.py
+++ api/main.py
@@ -1,8 +1,14 @@
 from contextlib import asynccontextmanager
+import json
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI, Depends
+from openai import OpenAI
 from sqlmodel import Session, select
 from api.database import init_db, get_session
 from api.models import Startup
+
+load_dotenv()
 
 
 @asynccontextmanager
@@ -13,6 +19,9 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(title="Startups API", lifespan=lifespan)
+client = OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY"),
+)
 
 
 @app.get("/startups/", response_model=list[Startup])
@@ -23,6 +32,32 @@ def list_startups(session: Session = Depends(get_session)):
 @app.get("/startups/{startup_id}", response_model=Startup)
 def get_startup(startup_id: int, session: Session = Depends(get_session)):
     startup = session.get(Startup, startup_id)
+    if not startup:
+        return {"error": "Startup not found"}
+    return startup
+
+
+@app.get("/lookup/startups/{startup_url}", response_model=Startup)
+def lookup_startup(startup_url: str, session: Session = Depends(get_session)):
+    with open("api/instruction.txt", "r", encoding="utf-8") as f:
+        instruction = f.read()
+    with open("api/input_template.txt", "r", encoding="utf-8") as f:
+        input_template = f.read()
+    input = f"{', '.join(startup_url)}: {input_template}"
+    response = client.responses.create(
+        model="gpt-5-mini",
+        reasoning={"effort": "low"},
+        instructions=instruction,
+        input=input,
+        store=True,
+        tools=[{"type": "web_search"}],
+        stream=False,
+    )
+    startup_info = json.loads(response.output_text)
+    startup = Startup.model_validate(startup_info)
+    session.add(startup)
+    session.commit()
+    session.refresh(startup)
     if not startup:
         return {"error": "Startup not found"}
     return startup
