9191dd60537eb51155872055d85c8fb52a97a944
Lydia HE (HTX) <lydia_he@htx.gov.sg>
2025-09-24 20:15:56 +0800

    Modified schema types in models.py so that model_validation behaves correctly

--- api/models.py
+++ api/models.py
@@ -3,35 +3,58 @@ from sqlmodel import SQLModel, Field, Column, JSON
 from sqlalchemy.dialects.postgresql import ENUM
 from pgvector.sqlalchemy import Vector
 
+
 class Startup(SQLModel, table=True):
     __tablename__ = "startups"
 
     id: Optional[int] = Field(default=None, primary_key=True)
     company_name: str
 
-    founders: Optional[Dict] = Field(sa_column=Column(JSON))
-    investors: Optional[List[str]] = Field(sa_column=Column(JSON))
+    founders: Optional[Dict] = Field(default=None, sa_column=Column(JSON))
+    investors: Optional[List[str]] = Field(default=None, sa_column=Column(JSON))
 
-    funding_stage: Optional[str] = Field(sa_column=Column(
-        ENUM(
-            "Conceptual", "Pre-seed", "Seed", "Series A", "Series B", "Series C", "Series D",
-            name="funding_stage", create_type=False
-        )
-    ))
+    funding_stage: Optional[str] = Field(
+        default=None,
+        sa_column=Column(
+            ENUM(
+                "Conceptual",
+                "Pre-seed",
+                "Seed",
+                "Series A",
+                "Series B",
+                "Series C",
+                "Series D",
+                name="funding_stage",
+                create_type=False,
+            )
+        ),
+    )
 
-    funds_raised: Optional[str] = Field(sa_column=Column(
-        ENUM(
-            "<$500K", "$500K-$1M", "$1M-$5M", "$5M-$10M", ">$10M",
-            name="funds_raised", create_type=False
-        )
-    ))
+    funds_raised: Optional[str] = Field(
+        default=None,
+        sa_column=Column(
+            ENUM(
+                "<$500K",
+                "$500K-$1M",
+                "$1M-$5M",
+                "$5M-$10M",
+                ">$10M",
+                name="funds_raised",
+                create_type=False,
+            )
+        ),
+    )
 
-    ref_funding: Optional[str] = None
+    ref_funding: Optional[List[str]] = Field(default=None, sa_column=Column(JSON))
 
     tech_offering: Optional[str] = None
-    ref_tech: Optional[str] = None
-    tech_embedding: Optional[List[float]] = Field(sa_column=Column(Vector(1536)))
+    ref_tech: Optional[List[str]] = Field(default=None, sa_column=Column(JSON))
+    tech_embedding: Optional[List[float]] = Field(
+        default=None, sa_column=Column(Vector(1536))
+    )
 
     uvp: Optional[str] = None
-    ref_uvp: Optional[str] = None
-    uvp_embedding: Optional[List[float]] = Field(sa_column=Column(Vector(1536)))
+    ref_uvp: Optional[List[str]] = Field(default=None, sa_column=Column(JSON))
+    uvp_embedding: Optional[List[float]] = Field(
+        default=None, sa_column=Column(Vector(1536))
+    )
