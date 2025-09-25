29f9e3046902e6763e2c0b39d66a8f7e6d37586b
Lydia HE (HTX) <lydia_he@htx.gov.sg>
2025-09-25 15:52:58 +0800

    Refactor\Moved startups functions to /router

--- api/main.py
+++ api/main.py
@@ -1,14 +1,11 @@
 from contextlib import asynccontextmanager
-import json
 import os
 from dotenv import load_dotenv
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from openai import OpenAI
-from sqlmodel import Session, select
-from api.database import init_db, get_session
-from api.models import Startup
-from api.routers import check_domain
+from api.database import init_db
+from api.routers import check_domain, startups
 
 load_dotenv()
 
@@ -36,42 +33,4 @@ app.add_middleware(
 )
 
 app.include_router(check_domain.router, prefix="/check-domain")
-
-
-@app.get("/startups/", response_model=list[Startup])
-def list_startups(session: Session = Depends(get_session)):
-    return session.exec(select(Startup)).all()
-
-
-@app.get("/startups/{startup_id}", response_model=Startup)
-def get_startup(startup_id: int, session: Session = Depends(get_session)):
-    startup = session.get(Startup, startup_id)
-    if not startup:
-        return {"error": "Startup not found"}
-    return startup
-
-
-@app.get("/lookup/startups/{startup_url}", response_model=Startup)
-def lookup_startup(startup_url: str, session: Session = Depends(get_session)):
-    with open("api/instruction.txt", "r", encoding="utf-8") as f:
-        instruction = f.read()
-    with open("api/input_template.txt", "r", encoding="utf-8") as f:
-        input_template = f.read()
-    input = f"{', '.join(startup_url)}: {input_template}"
-    response = client.responses.create(
-        model="gpt-5-mini",
-        reasoning={"effort": "low"},
-        instructions=instruction,
-        input=input,
-        store=True,
-        tools=[{"type": "web_search"}],
-        stream=False,
-    )
-    startup_info = json.loads(response.output_text)
-    startup = Startup.model_validate(startup_info)
-    session.add(startup)
-    session.commit()
-    session.refresh(startup)
-    if not startup:
-        return {"error": "Startup not found"}
-    return startup
+app.include_router(startups.router, prefix="/startups")
---
+++ api/routers/startups.py
@@ -0,0 +1,58 @@
+from dotenv import load_dotenv
+from fastapi import APIRouter, Query
+import json
+from fastapi import Depends
+from openai import OpenAI
+import os
+from sqlmodel import Session, select
+from api.database import get_session
+from api.models import Startup
+
+load_dotenv()
+
+router = APIRouter()
+
+client = OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY"),
+)
+
+
+@router.get("/", response_model=list[Startup])
+def list_startups(session: Session = Depends(get_session)):
+    return session.exec(select(Startup)).all()
+
+
+@router.get("/{startup_id}", response_model=Startup)
+def get_startup(startup_id: int, session: Session = Depends(get_session)):
+    startup = session.get(Startup, startup_id)
+    if not startup:
+        return {"error": "Startup not found"}
+    return startup
+
+
+@router.get("/lookup", response_model=Startup)
+def lookup_startup(
+    startup_url: str = Query(...), session: Session = Depends(get_session)
+):
+    with open("api/instruction.txt", "r", encoding="utf-8") as f:
+        instruction = f.read()
+    with open("api/input_template.txt", "r", encoding="utf-8") as f:
+        input_template = f.read()
+    input = f"{', '.join(startup_url)}: {input_template}"
+    response = client.responses.create(
+        model="gpt-5-mini",
+        reasoning={"effort": "low"},
+        instructions=instruction,
+        input=input,
+        store=True,
+        tools=[{"type": "web_search"}],
+        stream=False,
+    )
+    startup_info = json.loads(response.output_text)
+    startup = Startup.model_validate(startup_info)
+    session.add(startup)
+    session.commit()
+    session.refresh(startup)
+    if not startup:
+        return {"error": "Startup not found"}
+    return startup
