6f364cb7bfde49d62218e222b9e7fd2598896031
Lydia HE (HTX) <lydia_he@htx.gov.sg>
2025-09-26 10:34:20 +0800

    Refactor\Cleaned up exploration code for startup data in frontend

--- greenfield/app/(with-navbar)/browse/page.tsx
+++ greenfield/app/(with-navbar)/browse/page.tsx
@@ -2,29 +2,8 @@
 
 import { useEffect, useState } from "react";
 import StartupTable from "./startup_table";
-import { StartupType, UserType } from "./startup-data-type";
+import { StartupType } from "./startup-data-type";
 import StartupDrawer from "./startup_drawer";
-
-const data: UserType[] = [
-    {
-        id: 1,
-        name: "Alice Johnson",
-        role: "Designer",
-        email: "alice@example.com",
-    },
-    {
-        id: 2,
-        name: "Mark Rivera",
-        role: "Developer",
-        email: "mark@example.com",
-    },
-    {
-        id: 3,
-        name: "Sophia Chen",
-        role: "Product Manager",
-        email: "sophia@example.com",
-    },
-];
 
 export default function BrowseStartups() {
     const [startups, setStartups] = useState<StartupType[]>([]);
--- greenfield/app/(with-navbar)/browse/startup-data-type.tsx
+++ greenfield/app/(with-navbar)/browse/startup-data-type.tsx
@@ -1,16 +1,3 @@
 import { components } from "@/app/types/api";
 
 export type StartupType = components["schemas"]["Startup"];
-
-export interface UserType {
-    id: number;
-    name: string;
-    role: string;
-    email: string;
-}
-
-interface StartupPreviewType
-    extends Pick<
-        StartupType,
-        "id" | "company_name" | "founders" | "company_website"
-    > {}
