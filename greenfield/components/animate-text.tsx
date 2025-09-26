49089c8cdc0375c1f5919aa5ef3ec778be421886
Lydia HE (HTX) <lydia_he@htx.gov.sg>
2025-09-26 16:04:29 +0800

    [UI]\Modified typing effect to be able to cancel previous active instances to prevent overlap.

--- greenfield/components/animate-text.tsx
+++ greenfield/components/animate-text.tsx
@@ -1,12 +1,29 @@
+import { useRef } from "react";
+
+function _resetTypingInterval(
+    typingIntervalRef?: React.RefObject<ReturnType<typeof setInterval> | null>
+) {
+    if (!typingIntervalRef?.current) return;
+    clearInterval(typingIntervalRef.current);
+    typingIntervalRef.current = null;
+}
+
 export function typingEffect(
     setText: (text: string) => void,
     fullText: string,
-    typingSpeed: number = 300
+    typingSpeed: number = 300,
+    typingIntervalRef?: React.RefObject<ReturnType<typeof setInterval> | null>
 ) {
+    _resetTypingInterval(typingIntervalRef);
+    typingIntervalRef = typingIntervalRef
+        ? typingIntervalRef
+        : useRef<ReturnType<typeof setInterval> | null>(null);
     let i = 0;
-    const interval = setInterval(() => {
+    typingIntervalRef.current = setInterval(() => {
         setText(fullText.slice(0, i + 1));
         i++;
-        if (i === fullText.length) clearInterval(interval);
+        if (i === fullText.length) {
+            _resetTypingInterval(typingIntervalRef);
+        }
     }, typingSpeed);
 }
